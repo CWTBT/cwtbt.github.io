ef39f5aaf18c35aee8cab382ef4c7791e819d91b
CWTBT <lw03232@gmail.com>
2019-10-24 22:19:38 -0500

    "implemented script, fixed css"

--- csci340/traffic/script.js
+++ csci340/traffic/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 
+  let color = "red";
+  $("#traffic").click(function() {
+    console.log(color)
+    if (color === "red") {color = "yellow"}
+    else if (color === "yellow") {color = "green"}
+    else {color = "red"}
+    $("#traffic").css("background-color", color)
+  });
 });
