73d5680260976e72b2b74e2c0f5f7c0a308cae5d
CWTBT <lw03232@gmail.com>
2019-10-31 19:18:12 -0500

    name generation working

--- csci340/labs/lab6/script.js
+++ csci340/labs/lab6/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  $.ajax({
-    dataType: "json",
-    jsonCallback: "getName",
-    url: "https://uinames.com/api/?amount=1",
-    success: function(results) {
-      $('.name').text(results["name"]);
-    },
-    error: function(xhr, status, error) {
-      console.log(error);
-    }
-  })
+  $(":button").click(function() {
+
+    $.ajax({
+      dataType: "json",
+      jsonCallback: "getName",
+      url: "https://cors-anywhere.herokuapp.com/https://uinames.com/api/?amount=1&region=nepal",
+      success: function(results) {
+        $('.name').text(results["name"]);
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+      }
+    });
+  });
 });
