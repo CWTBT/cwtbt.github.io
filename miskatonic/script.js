bba9269e34c3a4bf2fb536138bf43364a107ee61
CWTBT <lw03232@gmail.com>
2020-03-31 04:05:13 -0500

    implemented dice rolling

--- miskatonic/script.js
+++ miskatonic/script.js
@@ -2,7 +2,25 @@ $(document).ready(function() {
     
     $("#charform").submit(function(e) {
         e.preventDefault();
-        console.log($(this).serialize());
+        console.log("Characteristics:" + rollCharacteristics());
       });
+
+      function rollCharacteristics() {
+        let physChars = [];
+        for (let i = 0; i < 5; i++) {
+            physChars.push(rollDice(3) * 5);
+          }
+        return physChars;
+      }
+
+      function rollDice(dice) {
+        let rollSum = 0;
+        for (let i = 0; i < dice; i++) {
+              let newRoll = (Math.floor(Math.random() * 6) + 1);
+              console.log("Roll: " + newRoll);
+              rollSum += newRoll;
+          }
+          return rollSum
+      }
 
 });
